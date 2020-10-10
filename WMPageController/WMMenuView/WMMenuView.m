82dc82a3e02d81fbca97260c9307c8b28edb9193
JiangYelinGit <897134699@qq.com>
2020-10-10 17:23:46 +0800

    Update WMMenuView.m

--- WMPageController/WMMenuView/WMMenuView.m
+++ WMPageController/WMMenuView/WMMenuView.m
@@ -534,7 +534,7 @@ static NSInteger const WMBadgeViewTagOffset = 1212;
 - (void)addProgressViewWithFrame:(CGRect)frame isTriangle:(BOOL)isTriangle hasBorder:(BOOL)hasBorder hollow:(BOOL)isHollow cornerRadius:(CGFloat)cornerRadius {
     WMProgressView *pView = [[WMProgressView alloc] initWithFrame:frame];
     pView.itemFrames = [self convertProgressWidthsToFrames];
-    pView.color = self.lineColor.CGColor;
+    pView.color = self.lineColor;
     pView.isTriangle = isTriangle;
     pView.hasBorder = hasBorder;
     pView.hollow = isHollow;
